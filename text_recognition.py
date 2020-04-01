7eef2aefd45c7fcd497eebd6e7a32b3ab0de6593
prghub123 <40703056+prghub123@users.noreply.github.com>
2020-03-31 19:12:03 -0500

    got access to aws api

---
+++ text_recognition.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import csv
+import boto3
+
+with open('credentials.csv', 'r') as input:
+    next(input)
+    reader = csv.reader(input)
+    for line in reader:
+        access_key = line[2]
+        password = line[3]
+img = '' #pass in image here
+boto3_client = boto3.client('rekognition', aws_access_key_id = access_key, aws_secret_access_key = password)
