5ffe302ac7cc78b5e77491dea688174c496c0d13
prg <gudappr@gmail.com>
2020-05-20 14:18:55 -0500

    Updates
    Finished AWS text recognition and started working on filtering text and producing sentiment

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,10 +2,45 @@ import os
 import sys
 import csv
 from text_recognition import text_recog
+from textblob import TextBlob
 import boto3
 
-#doing some testing 
-test = text_recog();
-test.open_img('blueberries.jpg')
-text = test.extract_text()
-print(text.keys())
+def recognize_text(picture):
+    test = text_recog();
+    test.open_img(picture)
+    text = test.extract_text()
+    return text
+
+#filter out individual words
+def filter_words(text):
+    text[:] = [x for x in text if not ('LINE' == x.get('Type'))]
+    #print(text)
+    #filter words based on confidence level
+    words = [x.get('DetectedText') for x in text if (x.get('Confidence') > 50)]
+    #print(words)
+    #POS-tagging to extract useful words
+    full_string = ""
+    for word in words:
+        full_string += (word + ' ')
+        #print(full_string)
+        blob = TextBlob(full_string)
+        tags = blob.tags #list of tuples (assigning POS tag to each word in string)
+        nouns = blob.noun_phrases #WordList of nouns present in string
+        #print(tags)
+        #print(nouns)
+
+#function to assess sentiment of text
+def sentiment(blob):
+    subject = blob.sentiment.subjectivity
+    polar = blob.sentiment.polarity
+    print("Subjectivity Rating: ", subject)
+    print("Polarity Rating: ", polar)
+    #assess subjectivity
+    if(subject < 0.1):
+        print("Source appears to be very objective")
+    if(subject > 0.1 and subject < 0.5):
+        print("Source appears to be fairly objective")
+    if(subject > 0.5 and subject < 0.9):
+        print("Source appears to be fairly subjective")
+    if(subject > 0.9):
+        print("Source appears to be very subjective")
--- text_recognition.py
+++ text_recognition.py
@@ -9,12 +9,11 @@ class text_recog():
 
     #retreving access key and password for AWS
     def __init__(self):
-        with open('credentials.csv', 'r') as input:
-            next(input)
-            reader = csv.reader(input)
-            for line in reader:
-                access_key = line[2]
-                password = line[3]
+        with open('rootkey.txt', 'r') as input:
+            lines = input.readlines() #creates list of lines in the txt file
+            access_key = lines[0]
+            password = lines[1]
+            access_key = access_key.replace('\n', '') #remove new line character from access_key
         #creating boto3 client
         self.boto3_client = boto3.client('rekognition', aws_access_key_id = access_key, aws_secret_access_key = password, region_name = 'us-west-2')
         self.img = []
@@ -28,10 +27,8 @@ class text_recog():
 
     #invokes AWS detect_text function to extract a dictionary with the text
     def extract_text(self):
-        #calls boto3 detect_text method
+        #calls boto3 detect_text method (returns a dictonary)
         text = self.boto3_client.detect_text(Image = {'Bytes' : self.bytes})
-        return text
-
-    #filter text by line or by word; one parameter should be false and other should be true
-    #input should be a dictionary returned by AWS boto3 client
-    #def filter_text(text, filter_line, filter_word):
+        #returns a list of text detected
+        words = text['TextDetections']
+        return words
